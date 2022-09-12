86dabb29fc9173f99492af8bc7c53390f8126f66
Thiago Lázaro <thiagolazaro2009@gmail.com>
2022-09-11 21:40:14 -0300

    Criando controller UpdateUserAvatarController

---
+++ src/modules/accounts/userCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+class UpdateUserAvatarController {
+  async handle(request: Request, response: Response) {}
+}
+
+export { UpdateUserAvatarController };
