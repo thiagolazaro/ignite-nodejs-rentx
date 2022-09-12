605226965b1981bbffbd675dfd8d9c7e5d3a795a
Thiago Lázaro <thiagolazaro2009@gmail.com>
2022-09-11 22:47:35 -0300

    Criando a route upload avatar e Controller

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
+import multer from "multer";
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticate";
 
 import { CreateUserController } from "../modules/accounts/userCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/userCases/updateUserAvatar/UpdateUserAvatarController";
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateUserAvatarController.handle);
 
 export { usersRoutes };
